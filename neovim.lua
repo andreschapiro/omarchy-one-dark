cb6ee60d7344faa03c97d0b18dc8983223bfab9d
Andy Chapiro <andy@andych.io>
2025-08-28 08:57:05 -0700

    Initial commit with a base working theme

---
+++ neovim.lua
@@ -0,0 +1,31 @@
+return {
+	"olimorris/onedarkpro.nvim",
+	name = "onedarkpro",
+	priority = 1000,
+	config = function()
+		require("onedarkpro").setup({
+			colors = {
+				bg = "#282c34",
+				fg = "#abb2bf",
+				red = "#ef596f",
+				orange = "#d19a66",
+				yellow = "#e5c07b",
+				green = "#89ca78",
+				cyan = "#2bbac5",
+				blue = "#61afef",
+				purple = "#d55fde",
+				white = "#abb2bf",
+				black = "#282c34",
+				gray = "#5c6370",
+				highlight = "#e2be7d",
+				comment = "#7f848e",
+				none = "NONE",
+			},
+			options = {
+				transparency = true,
+				lualine_transparency = true,
+			},
+		})
+		vim.cmd.colorscheme("onedark_vivid")
+	end,
+}
